8371f156df914a80d08bbbf4b527e15eed3a32bb
joshuadonatien <147733641+joshuadonatien@users.noreply.github.com>
2024-10-17 20:23:37 -0400

    Question 2

--- LSP1_Fall_2024/src/org/howard/edu/lsp/midterm/question2/Calculator.java
+++ LSP1_Fall_2024/src/org/howard/edu/lsp/midterm/question2/Calculator.java
@@ -22,16 +22,3 @@ public class Calculator {
     }
 }
 
-// This is the driver class to test the Calculator class
-public class CalculatorDriver {
-    public static void main(String[] args) {
-        // Test adding two integers
-        System.out.println(Calculator.sum(5, 10));  // Output: 15
-        
-        // Test adding two double numbers
-        System.out.println(Calculator.sum(3.5, 7.8));  // Output: 11.3
-        
-        // Test adding up an array of integers
-        System.out.println(Calculator.sum(new int[]{1, 2, 3, 4, 5}));  // Output: 15
-    }
-}
